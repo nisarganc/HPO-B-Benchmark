5a495174c3ea883e9a6908db425c053e79bfd1a0
nisarganc <nisarganc7@gmail.com>
2023-12-31 20:31:36 +0100

    readme and benchmark

--- example_vaetbenchmark.py
+++ example_vaetbenchmark.py
@@ -11,20 +11,19 @@ if __name__ == "__main__":
     name = "vaet_benchmark"
     new_method_name = "VAET.json"
     experiments = ["Random", "DGP", "GP", "VAET"]
-    n_trials = 10
+    n_trials = 50
     torch_seed = 999
 
     benchmark_plotter  = BenchmarkPlotter(experiments = experiments, 
-                                          seeds=["test1", "test2", "test3", "test4"],
                                           name = name,
                                           n_trials = n_trials,
-                                            results_path = results_path, 
-                                            output_path = output_path, 
-                                            data_path = data_path, 
-                                            search_spaces=['5971'])
+                                          results_path = results_path, 
+                                          output_path = output_path, 
+                                          data_path = data_path, 
+                                          search_spaces=['5971'])
 
     benchmark_plotter.generate_rank_and_regret()
-    benchmark_plotter.generate_plots_per_search_space()
+    benchmark_plotter.generate_aggregated_plots()
 
 
 
